adabbc185d7659b6d84125647dec7872126f728e
Tyler <tiller1010@gmail.com>
2020-02-09 21:48:31 -0500

    Bootstrap preset

--- resources/views/Messages/create.blade.php
+++ resources/views/Messages/create.blade.php
@@ -4,10 +4,14 @@
 	<div>
 		<form action="/messages" method="POST">
 			@csrf
-			<label for="To">To:</label>
-			<input type="text" name="To" />
-			<label for="message">Message:</label>
-			<textarea name="Message"></textarea>
+			<div class="form-group">
+				<label for="To">To:</label>
+				<input type="text" name="To" class="form-control" />
+			</div>
+			<div class="form-group">
+				<label for="message">Message:</label>
+				<textarea name="Message" class="form-control" ></textarea>
+			</div>
 			<input type="submit" value="Send">
 		</form>
 	</div>
--- resources/views/layouts/inbox.blade.php
+++ resources/views/layouts/inbox.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>Inbox</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
 	<h1>Inbox</h1>
