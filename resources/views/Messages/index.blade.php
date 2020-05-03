aa2e4d0aef7ac11bb74314775c51d7049cd773a5
Tyler <tiller1010@gmail.com>
2020-05-03 10:44:26 -0400

    No messages create button

--- resources/views/Messages/index.blade.php
+++ resources/views/Messages/index.blade.php
@@ -2,32 +2,34 @@
 
 @section('content')
 
-<div class="position-sticky py-1 message-heading">
-	<h2>All Messages:</h2>
-</div>
-@foreach($mostRecentConversationMessages as $m)
-@if($user->can('view', $m))
-	@if($m->From === auth()->user()->name)
-	<div style="margin-bottom: 40px;" class="alert alert-dark">
-		<h4>{{$m->To}}</h4>
-	@else
-	<div style="margin-bottom: 40px;" class="alert alert-info">
-		<h4>{{$m->From}}</h4>
-	@endif
-		<p>{{$m->Message}}</p>
-		@if($m->Read)
-			<p>Read</p>
-		@else
-			<p>Unread</p>
-		@endif
-		@if($user->id === $m->From_user_id)
-		<a class="btn btn-primary" href="messages/{{$m->To_user_id}}">View</a>
-		@else
-		<a class="btn btn-primary" href="messages/{{$m->From_user_id}}">View</a>
-		@endif
+@if($mostRecentConversationMessages->count() > 0)
+	<div class="position-sticky py-1 message-heading">
+		<h2>All Messages:</h2>
 	</div>
+	@foreach($mostRecentConversationMessages as $m)
+	@if($user->can('view', $m))
+		@if($m->From === auth()->user()->name)
+		<div style="margin-bottom: 40px;" class="alert alert-dark">
+			<h4>{{$m->To}}</h4>
+		@else
+		<div style="margin-bottom: 40px;" class="alert alert-info">
+			<h4>{{$m->From}}</h4>
+		@endif
+			<p>{{$m->Message}}</p>
+			@if($m->Read)
+				<p>Read</p>
+			@else
+				<p>Unread</p>
+			@endif
+			@if($user->id === $m->From_user_id)
+			<a class="btn btn-primary" href="messages/{{$m->To_user_id}}">View</a>
+			@else
+			<a class="btn btn-primary" href="messages/{{$m->From_user_id}}">View</a>
+			@endif
+		</div>
+	@endif
+	@endforeach
 @endif
-@endforeach
 
 @include('Messages.create')
 
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,6 +62,10 @@
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
+
+                                @if (Route::has('register'))
+                                    <a class="btn btn-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                @endif
                             </div>
                         </div>
                     </form>
