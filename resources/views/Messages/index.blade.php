650c3d07458ad54607e94e17f9975fdfac942574
Tyler <tiller1010@gmail.com>
2020-05-21 20:01:48 -0400

    Button update

--- resources/views/Messages/index.blade.php
+++ resources/views/Messages/index.blade.php
@@ -8,11 +8,11 @@
 	</div>
 	@foreach($mostRecentConversationMessages as $m)
 	@if($user->can('view', $m))
-		@if($m->From === auth()->user()->name)
-		<div style="margin-bottom: 40px;" class="alert alert-dark">
+		@if($user->id === $m->From_user_id)
+		<a href="messages/{{$m->To_user_id}}" class="index-message alert alert-dark">
 			<h4>{{$m->To}}</h4>
 		@else
-		<div style="margin-bottom: 40px;" class="alert alert-info">
+		<a href="messages/{{$m->From_user_id}}" class="index-message alert alert-info">
 			<h4>{{$m->From}}</h4>
 		@endif
 			<p>{{$m->Message}}</p>
@@ -21,12 +21,7 @@
 			@else
 				<p>Unread</p>
 			@endif
-			@if($user->id === $m->From_user_id)
-			<a class="btn btn-primary" href="messages/{{$m->To_user_id}}">View</a>
-			@else
-			<a class="btn btn-primary" href="messages/{{$m->From_user_id}}">View</a>
-			@endif
-		</div>
+		</a>
 	@endif
 	@endforeach
 @endif
